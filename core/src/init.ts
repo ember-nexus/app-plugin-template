78ef778b27d3591d832096a8bb9916d2c41f8469
Syndesi <soerenklein98@protonmail.com>
2026-01-23 22:35:44 +0100

    wip ci

--- core/src/init.ts
+++ core/src/init.ts
@@ -1,6 +1,10 @@
-function init(): void {
+function init(serviceRegistry: unknown): void {
   // eslint-disable-next-line no-console
   console.log('template plugin: init function called');
+  // eslint-disable-next-line no-console
+  console.log('service registry:');
+  // eslint-disable-next-line no-console
+  console.log(serviceRegistry);
 }
 
 export { init };
