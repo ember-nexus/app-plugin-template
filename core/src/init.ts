5434a379d3cc3e02a9e6732a0f8f6630259f9794
Syndesi <soerenklein98@protonmail.com>
2026-01-25 10:09:23 +0100

    add wip integration with `@ember-nexus/app-core`

--- core/src/init.ts
+++ core/src/init.ts
@@ -1,10 +1,15 @@
-function init(serviceRegistry: unknown): void {
+import { GetElementEndpoint } from '@ember-nexus/app-core/Endpoint/Element';
+import { ServiceResolver } from '@ember-nexus/app-core/Service';
+
+function init(serviceResolver: ServiceResolver): void {
   // eslint-disable-next-line no-console
   console.log('template plugin: init function called');
   // eslint-disable-next-line no-console
-  console.log('service registry:');
+  console.log('service resolver:');
   // eslint-disable-next-line no-console
-  console.log(serviceRegistry);
+  console.log(serviceResolver);
+  // eslint-disable-next-line no-console
+  console.log(serviceResolver?.getService(GetElementEndpoint.identifier));
 }
 
 export { init };
