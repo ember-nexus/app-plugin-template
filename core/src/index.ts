814fe8eee491d76af22ee28c36e7d6a28ee85368
Syndesi <soerenklein98@protonmail.com>
2026-02-01 23:00:05 +0100

    fix css export

--- core/src/index.ts
+++ core/src/index.ts
@@ -1,7 +1,8 @@
 export * as Component from './Component/index.js';
-export * as indexStyles from './index.css?inline';
 export * from './init.js';
 export * from './optimizeDynamicConfigurations.js';
 export * as Page from './Page/index.js';
 
 import './index.css';
+import indexStyles from './index.css?inline';
+export { indexStyles };
